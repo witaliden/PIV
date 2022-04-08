df4a63d600e2bac45c9605324eb69af9a8ab2848
Aukvito <aukvito@gmail.com>
2022-04-08 10:15:11 +0200

    initial commit

---
+++ source/repos/P4/P4_0327/Program.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using P4_0327;
+using System.Data.SqlClient;
+
+var cstring = @"Data Source=DESKTOP-703RFM4\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True";
+using var conn = new SqlConnection(cstring);
+
+conn.Open();
+int newNum = Convert.ToInt16(Console.ReadLine());
+string? newDesc = Console.ReadLine().Trim();
+string insertString = "INSERT into dbo.Region (RegionID, RegionDescription) VALUES ('" + newNum + "','" + newDesc + "' )";
+SqlCommand insert1 = new(insertString, conn);
+insert1.ExecuteNonQuery();
+
+var regions = conn.Query<Region>("Select * FROM Region");
+
+foreach (var item in regions)
+{
+    Console.WriteLine($"{item.RegionID}");
+}
+
+conn.Close();
---
+++ source/repos/P4/P4_0327/Region.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_0327
+{
+    internal class Region
+    {
+        public int RegionID;
+        public string RegionName;
+    }
+}
