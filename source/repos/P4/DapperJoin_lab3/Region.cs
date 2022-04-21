d76241bd9d00a13011e0b370391367f540d75cd8
WD <52528405+witaliden@users.noreply.github.com>
2022-04-21 20:04:03 +0200

    Projekt z lab3

---
+++ source/repos/P4/DapperJoin_lab3/Region.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DapperJoin_lab3;
+internal class Region
+{
+    public int RegionId;
+    public string? RegionDescription;
+}
---
+++ source/repos/P4/DapperJoin_lab3/Territories.cs
@@ -0,0 +1,9 @@
+﻿namespace DapperJoin_lab3;
+public class Territories
+{
+    //public string RegionDescription;
+    public int TerritoryId;
+    public string? TerritoryDescription;
+    public int RegionId;
+    public string? RegionDescription;
+}
