bab65c8a34a17398eedfc831b3f44437acea590c
WD <52528405+witaliden@users.noreply.github.com>
2022-04-21 16:50:57 +0200

    Użyłem SQLparameters by uniknąć SQL injection

--- source/repos/P4/P4_0327/Program.cs
+++ source/repos/P4/P4_0327/Program.cs
@@ -2,15 +2,21 @@
 using P4_0327;
 using System.Data.SqlClient;
 
-var cstring = @"Data Source=DESKTOP-703RFM4\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True";
+var cstring = @"Data Source=DESKTOP-JJ2EHR8\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True";
 using var conn = new SqlConnection(cstring);
 
 conn.Open();
 int newNum = Convert.ToInt16(Console.ReadLine());
-string? newDesc = Console.ReadLine().Trim();
-string insertString = "INSERT into dbo.Region (RegionID, RegionDescription) VALUES ('" + newNum + "','" + newDesc + "' )";
+var newDesc = Console.ReadLine()?.Trim();
+var insertString = "INSERT into dbo.Region (RegionID, RegionDescription) VALUES (@newNum, @newDesc)";
 SqlCommand insert1 = new(insertString, conn);
-insert1.ExecuteNonQuery();
+insert1.Parameters.AddWithValue("@newNum", newNum);
+insert1.Parameters.AddWithValue("@newDesc", newDesc);
+int result = insert1.ExecuteNonQuery();
+
+// Check Error
+if(result < 0)
+    Console.WriteLine("Error inserting data into Database!");
 
 var regions = conn.Query<Region>("Select * FROM Region");
 
