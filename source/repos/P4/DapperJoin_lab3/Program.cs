d0868af8cac535f0ce5adb4930f820a131611f89
WD <52528405+witaliden@users.noreply.github.com>
2022-05-13 16:45:50 +0200

    Dodano filtrowanie regionów po pierwszej literze

--- source/repos/P4/DapperJoin_lab3/Program.cs
+++ source/repos/P4/DapperJoin_lab3/Program.cs
@@ -5,17 +5,21 @@ using DapperJoin_lab3;
 
 var cstring = @"Data Source=DESKTOP-JJ2EHR8\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True";
 
-var sql = "SELECT TerritoryId, TerritoryDescription, t.RegionId, r.RegionDescription FROM Territories t INNER JOIN Region r ON t.RegionId = r.RegionId";
-var conn = new SqlConnection(cstring); 
+var sql = "SELECT TerritoryId, TerritoryDescription, t.RegionId, r.RegionDescription FROM Territories t INNER JOIN Region r ON t.RegionId = r.RegionId WHERE t.TerritoryDescription LIKE CONCAT (@letter, '%')";
+var conn = new SqlConnection(cstring);
+
+Console.WriteLine("Podaj pierwszą literę regionu: ");
+var userLetter = Console.ReadLine()?.Trim();
 
 var result = conn.Query<Territories, Region, Territories>(sql, (territory, region) =>
 {
     territory.RegionDescription = region.RegionDescription;
     return territory;
-}, splitOn: "RegionId");
+}, new {letter = userLetter},
+    splitOn: "RegionId");
 
 foreach (var item in result)
 {
     Console.WriteLine(
-        $"{item.TerritoryId} {item.TerritoryDescription} {item.RegionId} {item.RegionDescription} "); // {item.RegionDescription}
+        $"{item.TerritoryId} {item.TerritoryDescription} {item.RegionId} {item.RegionDescription} ");
 }
